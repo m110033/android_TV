0244ca878e3843d8dcce10136b48472219a45c19
Eric Tsai <erictsai@qnap.com>
2021-07-09 16:17:10 +0800

    Update the gparser

--- crawler_code/lib/gamer.py
+++ crawler_code/lib/gamer.py
@@ -35,39 +35,33 @@ def crawl_gamer(debug_mode, main_logger):
         # Get source
         html = request_func(video_url)
         html = html.replace('</i>', '')
-        # print(html)
         soup = BeautifulSoup(html, 'html.parser')
-        for ultags in soup.find_all('ul', {'class': 'anime_list'}):
-            for litags in ultags.find_all('li'):
-                page_link = "http://ani.gamer.com.tw/" + litags.find('a').get("href").strip()
-                img = litags.find('div', {'class': 'pic lazyload'}).get('data-bg').strip()
-                divtag = litags.find('div', {'class': 'info'})
-                year_info = str_between(divtag.text, "年份：", "共").strip()
-                try:
-                    date_obj = datetime.strptime(year_info, '%Y/%m')
-                    date_str = datetime.strftime(date_obj, '%Y-%m-%d')
-                except:
-                    date_str = "1911-01-01"
-                title = divtag.find('b').text.strip()
+        for atags in soup.find_all('a', {'class': 'theme-list-main'}):
+            page_link = "http://ani.gamer.com.tw/" + atags.get('href').strip()
+            title = atags.find('p', {'class': 'theme-name'}).getText()
+            img = str_between(atags.find('div', {'class': 'theme-img-bg'}).get('style'), '"', '"')
+            yearTxt = atags.find('p', {'class': 'theme-time'}).getText() + "共"
+            year_info = str_between(yearTxt, "年份：", "共").strip()
+            try:
+                date_obj = datetime.strptime(year_info, '%Y/%m')
+                date_str = datetime.strftime(date_obj, '%Y-%m-%d')
+            except:
+                date_str = "1911-01-01"
+            # print({
+            #     "title": title,
+            #     "img": img,
+            #     "page_link": page_link,
+            #     "date_str": date_str
+            # })
 
-                # print({
-                #     "title": title,
-                #     "img": img,
-                #     "page_link": page_link,
-                #     "date_str": date_str
-                # })
-
-                movie_obj.addMovie(
-                    movie_title = title,
-                    movie_image = img,
-                    movie_page_link = page_link,
-                    movie_second_title = date_str
-                )
-                continue
-                #break
+            movie_obj.addMovie(
+                movie_title = title,
+                movie_image = img,
+                movie_page_link = page_link,
+                movie_second_title = date_str
+            )
+            # break
         # break
-    # movie_obj.remove_unused_uuid(uuid_list)
-
     json_str = movie_obj.dictToJson()
 
     if debug_mode:
